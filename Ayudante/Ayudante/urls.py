dda94f2002420490e5fa6b9cf70cd99eba002410
eersya <eersyaando@gmail.com>
2020-09-11 20:13:21 -0500

    un paso más cerca

--- Ayudante/Ayudante/urls.py
+++ Ayudante/Ayudante/urls.py
@@ -1,4 +1,4 @@
-"""el_sitio URL Configuration
+"""Ayudante URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import render
+
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   # path('', render(template_name='home.html')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='')),
 ]
--- Ayudante/Ayudante/wsgi.py
+++ Ayudante/Ayudante/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for el_sitio project.
+WSGI config for Ayudante project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'el_sitio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ayudante.settings')
 
 application = get_wsgi_application()
