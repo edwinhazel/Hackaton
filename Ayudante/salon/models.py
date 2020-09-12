4cc2e25381404cf7a73a8d8e47172e8feeec1b07
eersya <eersyaando@gmail.com>
2020-09-11 23:28:19 -0500

    primera parte de los modelos

---
+++ Ayudante/salon/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Salon(models.Model):
+    clave = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return "clave:%d id_user:%d" % (self.clave, self.id_user)
+
+class Estudiante(models.Model):
+    id_user = models.IntegerField(primary_key=True)
+    salon = models.ForeignKey(Salon, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "id_user:%d clave_salon:%d" % (self.id_estudiante, self.salon)
+
+
+class Clase(models.Model):
+    id_clase = models.IntegerField(primary_key=True)
+    salon = models.ForeignKey(Salon, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "id_clase:%d clave_salon:%d" % (self.id_clase, self.clave_salon)
